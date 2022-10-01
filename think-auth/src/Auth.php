45bc097076bb686f951cf17bf662a7088c0fad95
AG <277493165@qq.com>
2022-10-01 16:41:09 +0800

    first commit

---
+++ think-auth/src/Auth.php
@@ -0,0 +1,12 @@
+<?php
+namespace agthink\auth;
+
+class Auth
+{
+    public function check(){
+        return 111;
+    }
+    public function login(){
+        return 222;
+    }
+}
---
+++ think-auth/src/facade/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace agthink\auth\facade;
+
+use think\Facade;
+
+class Auth extends Facade
+{
+//    protected static function getFacadeClass()
+//    {
+//        return 'agthink\auth\Auth';
+//    }
+
+    public function aa(){
+        return 2;
+    }
+
+}
